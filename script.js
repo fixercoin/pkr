8e424f1c0e5ab0f2bcf7b14782397466903787c5
fixercoin <khanbabusargodhaa@gmail.com>
2025-02-01 19:18:49 +0500

    script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let balance = parseFloat(localStorage.getItem('balance')) || 0.0000; // Initialize balance from local storage
-let clickBonus = 10; // Default click bonus
+let clickBonus = 5.30; // Default click bonus
 let isBonusActive = false; // Track if bonus is active
 let bonusDuration = 0; // Time left for bonus
 
@@ -15,7 +15,7 @@ if (lastBonusDate !== today) {
 document.getElementById('balance').innerText = balance.toFixed(4); // Update displayed balance
 
 document.getElementById('mineButton').addEventListener('click', () => {
-    balance += 10; // Increment balance by 0.0001 PKR
+    balance += 5.30; // Increment balance by 0.0001 PKR
     localStorage.setItem('balance', balance); // Save balance to local storage
     document.getElementById('balance').innerText = balance.toFixed(4); // Update displayed balance
 });
