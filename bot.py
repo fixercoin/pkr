255f1b2e50aade0cdc0a03a74ee3256a89d557c7
fixercoin <khanbabusargodhaa@gmail.com>
2025-02-04 02:55:02 +0500

    bot.py

--- bot.py
+++ bot.py
@@ -15,3 +15,24 @@ import db
 logging.basicConfig(level=logging.INFO)
 bot = Bot(token=config.BOT_TOKEN)
 dp = Dispatcher(bot)
+@dp.message_handler(commands=['start', 'help'])
+async def welcome_handler(message: types.Message):
+    uid = message.from_user.id  # Not neccessary, just to make code shorter
+
+    # If user doesn't exist in database, insert it
+    if not db.check_user(uid):
+        db.add_user(uid)
+
+    # Keyboard with two main buttons: Deposit and Balance
+    keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
+    keyboard.row(KeyboardButton('Deposit'))
+    keyboard.row(KeyboardButton('Balance'))
+
+    # Send welcome text and include the keyboard
+    await message.answer('FIXER DIGITAL COIN  '
+                         'made for [this article](docs.ton.org/v3/guidelines/dapps/tutorials/telegram-bot-examples/accept-payments-in-a-telegram-bot-2).\n'
+                         'My goal is to show how simple it is to receive '
+                         'payments in Toncoin with Python.\n\n'
+                         'Use keyboard to test my functionality.',
+                         reply_markup=keyboard,
+                         parse_mode=ParseMode.MARKDOWN)
