f522ccc7c7a07634d368c1d05173ffa1f586d654
fixercoin <khanbabusargodhaa@gmail.com>
2025-02-04 02:51:13 +0500

    bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,17 @@
+# Logging module
+import logging
 
+# Aiogram imports
+from aiogram import Bot, Dispatcher, types
+from aiogram.dispatcher.filters import Text
+from aiogram.types import ParseMode, ReplyKeyboardMarkup, KeyboardButton, \
+                          InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram.utils import executor
+
+# Local modules to work with the Database and TON Network
+import config
+import ton
+import db
+logging.basicConfig(level=logging.INFO)
+bot = Bot(token=config.BOT_TOKEN)
+dp = Dispatcher(bot)
