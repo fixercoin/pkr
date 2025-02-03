30f1d4e3f709c32593dc506f48c5efc7a18a9994
fixercoin <khanbabusargodhaa@gmail.com>
2025-02-04 01:52:21 +0500

    db.py

--- db.py
+++ db.py
@@ -1,3 +1,13 @@
 import sqlite3
 con = sqlite3.connect('db.sqlite')
 cur = con.cursor()
+cur.execute('''CREATE TABLE IF NOT EXISTS Users (
+                uid INTEGER,
+                balance INTEGER
+            )''')
+con.commit()
+def add_user(uid):
+    # new user always has balance = 0
+    cur.execute(f'INSERT INTO Users VALUES ({uid}, 0)')
+    con.commit()
+  
