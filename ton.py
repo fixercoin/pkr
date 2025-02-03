1b827e5cd99b11d1bf1e5fab9307a934dc033705
fixercoin <khanbabusargodhaa@gmail.com>
2025-02-04 01:58:05 +0500

    ton.py

--- ton.py
+++ ton.py
@@ -1,1 +1,41 @@
+{
+  "ok": true,
+  "result": [
+    {
+      ...
+    },
+    {
+      ...
+    }
+  ]
+}
+{
+    "@type": "raw.transaction",
+    "utime": 1666648337,
+    "data": "...",
+    "transaction_id": {
+        "@type": "internal.transactionId",
+        "lt": "32294193000003",
+        "hash": "ez3LKZq4KCNNLRU/G4YbUweM74D9xg/tWK0NyfuNcxA="
+    },
+    "fee": "105608",
+    "storage_fee": "5608",
+    "other_fee": "100000",
+    "in_msg": {
+        "@type": "raw.message",
+        "source": "EQBIhPuWmjT7fP-VomuTWseE8JNWv2q7QYfsVQ1IZwnMk8wL",
+        "destination": "EQBKgXCNLPexWhs2L79kiARR1phGH1LwXxRbNsCFF9doc2lN",
+        "value": "100000000",
+        "fwd_fee": "666672",
+        "ihr_fee": "0",
+        "created_lt": "32294193000002",
+        "body_hash": "tDJM2A4YFee5edKRfQWLML5XIJtb5FLq0jFvDXpv0xI=",
+        "msg_data": {
+            "@type": "msg.dataText",
+            "text": "SGVsbG8sIHdvcmxkIQ=="
+        },
+        "message": "Hello, world!"
+    },
+    "out_msgs": []
+}
 
